254421278fa0e7e13d9e24041c5e5400e2edab11
MaxChevalier <maxime.chevalier@ynov.com>
2023-10-03 10:55:37 +0200

    vue admin des table ticket et catégorie

--- ticket/admin.py
+++ ticket/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'createur', 'date_creation', 'data_cloture', 'etat')
